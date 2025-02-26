9edf514a3739dfcb3bd4678790ceb9166a3a5a29
assane05e <gueyeazou201629@gmaile.com>
2025-02-26 20:49:53 +0000

    message

---
+++ algo/app.js
@@ -0,0 +1,12 @@
+// ecrire un programme qui permet de calculer la somme de deux nombre
+
+// somme
+// deux nombre
+// calculer
+
+var n = 10,
+  b = 45;
+s = n + b;
+console.log(s); //d'afficher un message sur la console de notre navigateur
+//document.write(s); // d'afficher sur la page
+//alert(s); // permet d'afficher une boite de dialogue
